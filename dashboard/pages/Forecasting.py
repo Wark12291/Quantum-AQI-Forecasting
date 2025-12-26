af1aff1c1f8a6ba38e06c93bf660dd7e1da93b48
Wark12291 <wark12291@gmail.com>
2025-12-26 14:35:13 +0530

    Update Forecasting.py

--- dashboard/pages/Forecasting.py
+++ dashboard/pages/Forecasting.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
 # Try to import TensorFlow LSTM (fallback if missing)
